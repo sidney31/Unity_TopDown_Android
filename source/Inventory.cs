68ef02258b8b6ae7bce6096efe6dc1585125f905
Sidney31 <surovbogdan05@gmail.com>
2022-11-21 09:57:05 +0300

    improve trade system

--- source/Inventory.cs
+++ source/Inventory.cs
@@ -11,9 +11,17 @@ public class Inventory : MonoBehaviour
     [SerializeField] private TMP_Text Stone—ountText;
     [SerializeField] public int Stone—ount;
     [SerializeField] private GameObject[] buttons;
+    [SerializeField] private GameObject[] desc;
     //[SerializeField] private TMP_Text[] textQueue;
     [SerializeField] private TMP_Text textField;
 
+    private void Start()
+    {
+        for (int i = 0; i < desc.Length; i++)
+        {
+            desc[i].SetActive(false);
+        }
+    }
     private void Update()
     {
         MushroomCountText.text = MushroomCount.ToString();
@@ -24,10 +32,11 @@ public class Inventory : MonoBehaviour
         Debug.Log(gameObject.name);
         for (int i = 0; i < buttons.Length; i++)
         {
+            desc[i].SetActive(false);
             if (button.name == buttons[i].name)
             {
-                textField.text = buttons[i].name;
-            } 
+                desc[i].SetActive(true);
+            }
         }
     }
 
